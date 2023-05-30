8410a0b453e9f63a334ac03bf0c74269b3f0116a
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-05-30 09:34:16 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-efficient efficdnetg effiecne effic
+efficient efficdnetg effiecne effi
 /* eslint-disable import/no-named-as-default */
 import { writeFile } from 'fs';
 import { promisify } from 'util';
